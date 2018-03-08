c6d81ff1eed8f8fc2ee833febe9ad5163c531e75
Gearcoded <cipherplain@yandex.com>
2018-03-08 15:12:24 +0300

    Herokufiction

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,10 +32,8 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
-  #config.active_support.deprecation = :log
 
   # Raise an error on page load if there are pending migrations.
-  #config.active_record.migration_error = :page_load
 
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
@@ -46,9 +44,6 @@ Rails.application.configure do
   config.assets.quiet = true
 
   # Raises error for missing translations
-  # config.action_view.raise_on_missing_translations = true
-
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  #config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
